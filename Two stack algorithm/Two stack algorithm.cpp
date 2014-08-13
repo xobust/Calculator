2393bc8cadc66eac696761ce72f4438a2eee3ec2
Alexander Bladh <alexander.bladh@hotmail.com>
2014-08-13 22:52:35 +0200

    tabs -> spaces , github tablength to long :-1:

--- Two stack algorithm/Two stack algorithm.cpp
+++ Two stack algorithm/Two stack algorithm.cpp
@@ -41,194 +41,194 @@ bool IsLeft(char o){
 }
 
 int Precedence(char o){ //operator rank (ordning)
-	switch (o)
-	{
-	case '+':
-		return 1;
-	case '-':
-		return 1;
-	case '/':
-		return 2;
-	case '*':
-		return 2;
-	case '^':
-		return 3;
-	}
+    switch (o)
+    {
+    case '+':
+        return 1;
+    case '-':
+        return 1;
+    case '/':
+        return 2;
+    case '*':
+        return 2;
+    case '^':
+        return 3;
+    }
     
-	return 0;
+    return 0;
 }
 
 //räknar ut resultatet av en operation
 //och lägger tillbaka det på värdes stacken
 bool domath()
 {
-	// popa operatorn och de senaste värdena
-	char c = output.front()[0];
-	output.pop();
-	cout << c << " ";
-	double a, b;
+    // popa operatorn och de senaste värdena
+    char c = output.front()[0];
+    output.pop();
+    cout << c << " ";
+    double a, b;
 
-	if(vals.empty())
-		return false;
+    if(vals.empty())
+        return false;
 
-	b = vals.top(); // numren kommer i omvänd ordning 
-	vals.pop();
+    b = vals.top(); // numren kommer i omvänd ordning 
+    vals.pop();
 
-	if(vals.empty()) // fix för negativa tal i början av en uträkning men inte senare
-		if(c=='-') 
-			a=0;
-		else
-			return false;
-	else
-	{
-		a = vals.top();
-		vals.pop();
-	}
+    if(vals.empty()) // fix för negativa tal i början av en uträkning men inte senare
+        if(c=='-') 
+            a=0;
+        else
+            return false;
+    else
+    {
+        a = vals.top();
+        vals.pop();
+    }
 
-	switch(c)
-	{
-	case '*':
-		vals.push(a*b);
-		break;
-	case '+':
-		vals.push(a+b);
-		break;
-	case '-':
-		vals.push(a-b);
-		break;
-	case '/':
-		vals.push(a/b);
-		break;
-	case '^':
-		vals.push(pow(a,b));
-		break;
-	}
+    switch(c)
+    {
+    case '*':
+        vals.push(a*b);
+        break;
+    case '+':
+        vals.push(a+b);
+        break;
+    case '-':
+        vals.push(a-b);
+        break;
+    case '/':
+        vals.push(a/b);
+        break;
+    case '^':
+        vals.push(pow(a,b));
+        break;
+    }
 
-	return true;
+    return true;
 }
 
 //Infix till omvänd polsk notation
 int ToReversePolish(string exp)
 {
-	exp.erase(remove_if(exp.begin(), exp.end(), isspace), exp.end());
+    exp.erase(remove_if(exp.begin(), exp.end(), isspace), exp.end());
 
-	for(int i = 0; i < exp.length(); i++)
-	{
-		if(isalpha(exp[i]))
-		{
-			if(i+1 < exp.length() && exp.substr(i,2) == "pi")
-			{	
-				output.push("3.14159265359"); 
-				i+=2; //gå förbi på
-			}
-			else
-			{
-				cout<<"unknown character detected: "<<exp[i]<<"\n";
-				break;
-			}
-			//todo implementera variabler och funktioner
+    for(int i = 0; i < exp.length(); i++)
+    {
+        if(isalpha(exp[i]))
+        {
+            if(i+1 < exp.length() && exp.substr(i,2) == "pi")
+            {	
+                output.push("3.14159265359"); 
+                i+=2; //gå förbi på
+            }
+            else
+            {
+                cout<<"unknown character detected: "<<exp[i]<<"\n";
+                break;
+            }
+            //todo implementera variabler och funktioner
 
-			if(i>=exp.length()) //om utrycket tar slut efter variabeln
-				break;
+            if(i>=exp.length()) //om utrycket tar slut efter variabeln
+                break;
 
-		}
-		if (isdigit(exp[i]))
-		{
-			string temp="";
-			
-			for(int j = i; j < exp.length(); j++)
-			{
-				 if(isdigit(exp[j]) || exp[j] == '.')
-				 {
-					 temp += exp[j];
-					 i = j;
-				 }
-				 else 
-				 {
-					break;
-				 }
-			}
+        }
+        if (isdigit(exp[i]))
+        {
+            string temp="";
+            
+            for(int j = i; j < exp.length(); j++)
+            {
+                 if(isdigit(exp[j]) || exp[j] == '.')
+                 {
+                     temp += exp[j];
+                     i = j;
+                 }
+                 else 
+                 {
+                    break;
+                 }
+            }
 
-			output.push(temp);
+            output.push(temp);
 
-		}else
-		{
-			if(exp[i] == '(')
-				oper.push('(');
-			else if( exp[i] == ')')
-			{
-				while(oper.top() != '(')
-				{
-					output.push(cts(oper.top()));
-					oper.pop();
-				}
-				oper.pop();
+        }else
+        {
+            if(exp[i] == '(')
+                oper.push('(');
+            else if( exp[i] == ')')
+            {
+                while(oper.top() != '(')
+                {
+                    output.push(cts(oper.top()));
+                    oper.pop();
+                }
+                oper.pop();
 
-			}else
-			{
-				char o1 = exp[i];
-				while (oper.size() > 0)
-				{
-					char o2 = oper.top();
-					if((IsLeft(o1)  && Precedence(o1) <= Precedence(o2))
+            }else
+            {
+                char o1 = exp[i];
+                while (oper.size() > 0)
+                {
+                    char o2 = oper.top();
+                    if((IsLeft(o1)  && Precedence(o1) <= Precedence(o2))
                        ||  (IsRight(o1) && Precedence(o1) <  Precedence(o2)))
-					{
-					   output.push(cts(o2));
-					   oper.pop();
-					  
-					}else
-					{
-						break;
-					}
-				}
-				 oper.push(o1);
-			}
-		}
-	}
+                    {
+                       output.push(cts(o2));
+                       oper.pop();
+                      
+                    }else
+                    {
+                        break;
+                    }
+                }
+                 oper.push(o1);
+            }
+        }
+    }
 
-	while(oper.size() > 0)
-	{
-		output.push(cts(oper.top()));
-		oper.pop();
-	}
+    while(oper.size() > 0)
+    {
+        output.push(cts(oper.top()));
+        oper.pop();
+    }
 
-	return 1;
+    return 1;
 }
 
 //Räknar ut den omvända polska notationen
 double calculate(string str)
 {
-	ToReversePolish(str);
-	cout << "Reverse polish result: ";
-	while(!output.empty())
-	{
-		string s = output.front();
-		if(s.length() > 1 || isalnum(s[0])) // om det är ett nummer lägg det på nummer stacken
-		{
-			double d = 0;
-			String2Double(s,d);
-			vals.push(d);
-			output.pop();
+    ToReversePolish(str);
+    cout << "Reverse polish result: ";
+    while(!output.empty())
+    {
+        string s = output.front();
+        if(s.length() > 1 || isalnum(s[0])) // om det är ett nummer lägg det på nummer stacken
+        {
+            double d = 0;
+            String2Double(s,d);
+            vals.push(d);
+            output.pop();
 
-			cout<< d << " ";
-		}else //annars gör en operation 
-		{
-			if(!domath())
-				cout<< "syntax error\n";
-		}
-	}
+            cout<< d << " ";
+        }else //annars gör en operation 
+        {
+            if(!domath())
+                cout<< "syntax error\n";
+        }
+    }
 
-	cout << "= \n";
-	return vals.top();
+    cout << "= \n";
+    return vals.top();
 }
 
 
 int main(int argc, _TCHAR* argv[])
 {
-	string s;
-	cin>>s;
-	cout << calculate(s); 
-	getch();
+    string s;
+    cin>>s;
+    cout << calculate(s); 
+    getch();
 
-	return 0;
+    return 0;
 }
